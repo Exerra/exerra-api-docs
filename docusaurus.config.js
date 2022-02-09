ed80466158bfe494ba8663bd96d9099bc32f2ea2
Exerra <exerra@exerra.xyz>
2022-02-10 00:39:02 +0200

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,119 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Exerra's API",
+  tagline: "Documentation for Exerra's API",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  presets: [
+    [
+      "docusaurus-preset-openapi",
+      /** @type {import('docusaurus-preset-openapi').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('docusaurus-preset-openapi').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Exerra's API",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          /*{
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },*/
+          { to: "/api", label: "API", position: "left" },
+          //{ to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/Exerra/exerra-api-docs",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "light",
+        links: [
+          {
+            title: "More",
+            items: [
+              {
+                label: "Status",
+                href: "https://status.exerra.xyz",
+              },
+              {
+                label: "Karen Bot",
+                href: "https://karen.exerra.xyz",
+              },
+              {
+                label: "Exerra's website",
+                href: "https://exerra.xyz",
+              },
+              {
+                label: "Exerra's mods",
+                href: "https://mods.exerra.xyz",
+              },
+            ],
+          },
+          {
+            title: "External",
+            items: [
+              {
+                label: "Discord",
+                href: "https://guild.exerra.xyz",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/Exerra",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/Exerra",
+              },
+            ],
+          }
+        ],
+        copyright: `Copyright © whatever year it is rn by Exerra`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures.js
@@ -0,0 +1,62 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
+
+const FeatureList = [
+  {
+    title: "Easy to Use",
+    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
+    description: (
+      <>
+        The Exerra API was designed from the ground up to be simple, and free.
+      </>
+    ),
+  },
+  {
+    title: "Focus on What Matters",
+    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
+    description: (
+      <>
+        Using a lot of complicated magic, the Exerra API let's you write as less code as possible. Instead of making these crazy scripts, everything is just one REST call away!
+      </>
+    ),
+  },
+  {
+    title: "Powered by React",
+    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+    ),
+  },
+];
+
+function Feature({ Svg, title, description }) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
